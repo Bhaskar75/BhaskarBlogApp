89af5e29fd2d2f8511dc4d6b20f98f42c11039db
Bhaskar Manna <34345805+Bhaskar75@users.noreply.github.com>
2025-01-07 01:43:19 +0530

    Completed third video of sec 5

---
+++ BhaskarBlogApp/BhaskarBlogApp/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,34 @@
+﻿using BhaskarBlogApp.Models.ViewModels;
+using BhaskarBlogApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BhaskarBlogApp.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var tags = await _tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.DisplayName, Value = x.Id.ToString() }),
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            return RedirectToAction("Add");
+        }
+    }
+}
