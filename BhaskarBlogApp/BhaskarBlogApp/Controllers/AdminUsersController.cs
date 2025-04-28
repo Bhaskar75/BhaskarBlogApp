cc4ba51fbd25143b394a126a488cbaba7d712806
Bhaskar Manna <34345805+Bhaskar75@users.noreply.github.com>
2025-04-28 18:34:29 +0530

    Completed section 10

--- BhaskarBlogApp/BhaskarBlogApp/Controllers/AdminUsersController.cs
+++ BhaskarBlogApp/BhaskarBlogApp/Controllers/AdminUsersController.cs
@@ -1,6 +1,7 @@
 ﻿using BhaskarBlogApp.Models.ViewModels;
 using BhaskarBlogApp.Repositories;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BhaskarBlogApp.Controllers
@@ -9,10 +10,14 @@ namespace BhaskarBlogApp.Controllers
     public class AdminUsersController : Controller
     {
         private readonly IUserRepository userRepository;
-        public AdminUsersController(IUserRepository userRepository)
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AdminUsersController(IUserRepository userRepository,UserManager<IdentityUser> userManager)
         {
             this.userRepository = userRepository;
+            this.userManager = userManager;
         }
+        [HttpGet]
         public async Task<IActionResult> List()
         {
             var users = await userRepository.GetAll();
@@ -28,6 +33,55 @@ namespace BhaskarBlogApp.Controllers
                 });
             }
             return View(usersViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> List(UserViewModel request)
+        {
+            var identyUser = new IdentityUser
+            {
+                UserName = request.Username,
+                Email = request.Email
+            };
+
+            var identityResult = await userManager.CreateAsync(identyUser, request.Password);
+
+            if (identityResult is not null)
+            {
+                if(identityResult.Succeeded)
+                {
+                    //assign roles to this user
+                    var roles = new List<string> { "User" };
+                    if(request.AdminRoleCheckBox)
+                    {
+                        roles.Add("Admin");
+                    }
+
+                    identityResult = await userManager.AddToRolesAsync(identyUser,roles);
+
+                    if (identityResult is not null && identityResult.Succeeded)
+                    {
+                        return RedirectToAction("List", "AdminUsers");
+                    }
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+            if (user != null)
+            {
+                var iR= await userManager.DeleteAsync(user);
+                if (iR != null && iR.Succeeded)
+                {
+                    return RedirectToAction("List", "AdminUsers");
+                }
+            }
+            return View();
         }
     }
 }
